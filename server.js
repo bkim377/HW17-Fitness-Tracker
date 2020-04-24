bbbdc64ef6d26810bb3870466eeb377994a9afe2
Brandon Kim <bkim377@gatech.edu>
2020-04-24 18:07:11 -0400

    added code for deploying to Heroku

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
